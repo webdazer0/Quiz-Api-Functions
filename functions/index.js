e7d6ceaff00a063c3bbf82ba81399bc73c065d62
Miguel C <webdazer0@gmail.com>
2021-05-15 03:09:29 +0200

    added express & cors

--- functions/index.js
+++ functions/index.js
@@ -1,4 +1,10 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+
+const express = require("express");
+
+const app = express();
+
+exports.app = app;
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
