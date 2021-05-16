b90fb1a0ebe78288c2cebf935d4b2e7afb0f1641
Miguel C <webdazer0@gmail.com>
2021-05-16 11:46:07 +0200

    added firebase admin, init CRUD (Create) & init firestore

--- functions/index.js
+++ functions/index.js
@@ -1,13 +1,32 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 
 const express = require("express");
 
 const app = express();
 
+admin.initializeApp({
+  credential: admin.credential.cert(require("./serviceAccountKey.json")),
+  databaseURL: "https://quiz-restful.firebaseio.com",
+});
+const db = admin.firestore();
+
 exports.app = functions.https.onRequest(app);
 
 app.get("/welcome", (req, res) => {
   res.status(200).json({ message: "Ciao Miguel" });
+});
+
+app.post("/api/questions", async (req, res) => {
+  const payload = {
+    question: req.body.question,
+    answers: req.body.answers,
+    correct_answer: req.body.correct_answer,
+  };
+
+  await db.collection("questions").doc().create(payload);
+
+  return res.status(200).json({ message: "Question Successfully Saved! =)" });
 });
 
 // // Create and Deploy Your First Cloud Functions
